c1afb085b1194a4c50a00728250c58fd98675ebe
salimtahayacine <salimtahayacine@gmail.com>
2025-01-15 12:13:17 +0100

    second commit

---
+++ src/main/java/com/example/demo/ProjectForSpringBootWithLiquibaseAndKeycloakAndMinioApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectForSpringBootWithLiquibaseAndKeycloakAndMinioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectForSpringBootWithLiquibaseAndKeycloakAndMinioApplication.class, args);
+	}
+
+}
